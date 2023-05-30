e5ffa748d94efa7aa04fd7a41629854599469f5c
FrankDian <1355253924@qq.com>
2023-05-30 10:12:30 +0800

    update: 调整代码结构

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Random } from './random';
+import Random from './random';
 
 const FMock = {
   Random,
@@ -6,17 +6,9 @@ const FMock = {
 
 export default FMock;
 
-// 支持生成对象、数组等复杂数据类型。
-// string/boolean/number/date/time/object/array
-// MOCK.mock('')
-
 // 支持自定义数据类型，例如生成身份证号码、手机号码等特定格式的数据。  
-// Mock.registerType() 1. 要防止覆盖已有 2.即为进行数据类型定义的流程的暴露
-
-// 支持生成符合特定规则的数据，例如生成指定范围内的数字、指定长度的字符串、指定格式的日期等
-// length:xxx min:xx max:xx template 
+// Mock.define() 1. 要防止覆盖已有 2.即为进行数据类型定义的流程的暴露
 
 // 支持根据数据模板生成数据，例如根据一个 JSON 模板生成符合该模板的数据。
-// ??? 没太看懂
 
 // 支持生成大量数据，例如生成 1000 条数据。
---
+++ src/random/index.ts
@@ -0,0 +1,17 @@
+import {
+  integer,
+  string,
+  boolean,
+  date,
+  time,
+} from './basic';
+
+const Random = {
+  integer,
+  string,
+  boolean,
+  date,
+  time,
+};
+
+export default Random;
